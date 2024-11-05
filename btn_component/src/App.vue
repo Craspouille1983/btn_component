8d0756efc8eaf11931d6edf8a46b900d220cda85
Craspouille <craspouille@craspouille>
2024-11-05 07:51:20 +0100

    Premier dépôt

---
+++ btn_component/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <buttonComponent modelValue="Je suis un bouton" />
+  <div v-for="button in buttons" :key="`button${button.id}`">
+    <buttonComponent :modelValue="button.text" />
+  </div>
+</template>
+
+<script>
+import buttonComponent from './components/btn_component.vue'
+
+export default {
+  name: 'App',
+  components: {
+    buttonComponent
+  },
+  data() {
+    return {
+      buttons: [
+        {"id": 1, "text": "Cliquer ici"},
+        {"id": 2, "text": "Cliquer là"},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
